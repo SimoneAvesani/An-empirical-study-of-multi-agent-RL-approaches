12ffc919dedb1a77d7b01268ad15152c67694117
SimoneAvesani <id530zug@studenti.univr.it>
2018-11-18 20:07:36 +0100

    Add files via upload

---
+++ MADDPG/wumpus_model.py
@@ -0,0 +1,141 @@
+import numpy as np
+from multiagent.core import World, Agent, Landmark
+from multiagent.scenario import BaseScenario
+
+class Scenario(BaseScenario):
+
+    def make_world(self):
+        world = World()
+        world.dim_c = 2
+        num_agents = 2
+        world.num_agents = num_agents
+        num_adversaries = 1
+        num_landmarks = 1
+        #add agents
+        world.agents = [Agent() for i in range(num_agents)]
+        for i, agent in enumerate(world.agents):
+            agent.name = 'agent %d' % i
+            agent.collide = True
+            agent.silent = True
+            agent.adversary = True if i <= num_adversaries else False
+            agent.size = 0.075 if agent.adversary else 0.05
+            agent.accel = 3.0 if agent.adversary else 4.0
+            agent.max_speed = 1.0 if agent.adversary else 1.3
+
+        #add landmark
+        world.landmarks = [Landmark() for i in range(1)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = 'landmark %d' % i
+            landmark.collide = True
+            landmark.movable = False
+            landmark.boundary= False
+            landmark.size = 0.2
+        #make initial conditions
+        self.reset_world(world)
+        return world
+
+    def reset_world(self, world):
+         # random properties for agents
+        for i, agent in enumerate(world.agents):
+            agent.color = np.array([0.35, 0.85, 0.35]) if not agent.adversary else np.array([0.85, 0.35, 0.35])
+            # random properties for landmarks
+        for i, landmark in enumerate(world.landmarks):
+            landmark.color = np.array([0.25, 0.25, 0.25])
+        # set random initial states
+        for agent in world.agents:
+            agent.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            agent.state.p_vel = np.zeros(world.dim_p)
+            agent.state.c = np.zeros(world.dim_c)
+        for i, landmark in enumerate(world.landmarks):
+            if not landmark.boundary:
+                landmark.state.p_pos = np.random.uniform(-0.9, +0.9, world.dim_p)
+                landmark.state.p_vel = np.zeros(world.dim_p)
+
+    def benchmark_data(self, agent, world):
+        # returns data for benchmarking purposes
+        if agent.adversary:
+            collisions = 0
+            for a in self.good_agents(world):
+                if self.is_collision(a, agent):
+                    collisions += 1
+            return collisions
+        else:
+            return 0
+    
+    def is_collision(self, agent1, agent2):
+        delta_pos = agent1.state.p_pos - agent2.state.p_pos
+        dist = np.sqrt(np.sum(np.square(delta_pos)))
+        dist_min = agent1.size + agent2.size
+        return True if dist < dist_min else False
+
+        # return all agents that are not adversaries
+    def good_agents(self, world):
+        return [agent for agent in world.agents if not agent.adversary]
+
+       # return all adversarial agents
+    def adversaries(self, world):
+        return [agent for agent in world.agents if agent.adversary]
+
+    def reward(self, agent, world):
+        # Agents are rewarded based on minimum agent distance to each landmark
+        return self.adversary_reward(agent, world) if agent.adversary else self.agent_reward(agent, world)
+
+    def agent_reward(self, agent, world):
+        # Agents are negatively rewarded if caught by adversaries
+        rew = 0
+        shape = True
+        landmark = world.landmarks[0]
+        adversaries = self.adversaries(world)
+        if shape:  # reward can optionally be shaped (increased reward for increased distance from adversary)
+            for adv in adversaries:
+                rew += 0.7 * np.sqrt(np.sum(np.square(agent.state.p_pos - adv.state.p_pos)))
+                rew -= 0.7 * np.sqrt(np.sum(np.square(agent.state.p_pos - landmark.state.p_pos)))
+        if agent.collide:
+            for a in adversaries:
+                if self.is_collision(a, agent):
+                    rew -= 100
+                    self.reset_world(world)
+        if agent.collide:
+            if self.is_collision(agent, landmark):
+                rew += 100
+                self.reset_world(world)
+
+        # agents are penalized for exiting the screen, so that they can be caught by the adversaries
+        def bound(x):
+            if x < 0.9:
+                return 0
+            if x < 1.0:
+                return (x - 0.9) * 10
+            return min(np.exp(2 * x - 2), 10)
+
+        for p in range(world.dim_p):
+            x = abs(agent.state.p_pos[p])
+            rew -= bound(x)
+        return rew
+
+
+    def adversary_reward(self, agent, world):
+        # Adversaries are rewarded for collisions with agents
+        rew = 0
+        shape = True
+        agents = self.good_agents(world)
+        adversaries = self.adversaries(world)
+        if shape:  # reward can optionally be shaped (decreased reward for increased distance from agents)
+            for adv in adversaries:
+                rew -= 0.7 * min([np.sqrt(np.sum(np.square(a.state.p_pos - adv.state.p_pos))) for a in agents])
+        if agent.collide:
+            for ag in agents:
+                for adv in adversaries:
+                    if self.is_collision(ag, adv):
+                        rew += 100
+                        self.reset_world(world)
+        return rew
+
+    def observation(self, agent, world):
+        # get positions of all entities in this agent's reference frame
+        entity_pos = []
+        for entity in world.landmarks:
+            if not entity.boundary:
+                entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+        # communication of all other agents
+        return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos)
